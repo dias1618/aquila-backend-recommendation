ba7a4e408e54a716c40681d9d5942e62bcd936b1
Gabriel Dias <dias.1618@gmail.com>
2021-02-12 14:35:57 -0300

    Update usuario.service.ts

--- src/services/usuario.service.ts
+++ src/services/usuario.service.ts
@@ -1,7 +1,6 @@
 import { Usuario } from "src/entities/usuario.entity";
 import { getRepository, Connection } from "typeorm";
 import { Injectable } from "@nestjs/common";
-import { UsuarioCategoria } from "src/entities/usuario-categoria.entity";
 
 @Injectable()
 export class UsuarioService{
@@ -13,11 +12,6 @@ export class UsuarioService{
         let usuario:Usuario = await getRepository(Usuario).createQueryBuilder('usuario')
             .where("usuario.id = :id", {id: idUsuario})
             .getOne();
-
-        usuario.usuariosCategorias = await getRepository(UsuarioCategoria).createQueryBuilder('usuario_categoria')
-            .where("usuario_categoria.usuarioId = :id", {id: idUsuario})
-            .innerJoinAndSelect("usuario_categoria.categoria", "categoria")
-            .getMany();
 
         return usuario;
     }
