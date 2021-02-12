03a051271cdd8b0ac5e1dba02bbf674a044e27c3
Gabriel Dias <dias.1618@gmail.com>
2021-02-12 00:30:08 -0300

    feature(recommendation): Adicionado canal e categoria na recomendacao

--- src/controllers/recommendation.controler.ts
+++ src/controllers/recommendation.controler.ts
@@ -10,7 +10,7 @@ export class RecommendationController {
 
     @Get()
     @HttpCode(200)
-    async recommendation(@Query('idUsuario') idUsuario) {
-        return await this.recommendationService.recomendarVideos(idUsuario); 
+    async recommendation(@Query('idUsuario') idUsuario, @Query('idCategoria') idCategoria, @Query('idCanal') idCanal) {
+        return await this.recommendationService.recomendarVideos(idUsuario, idCategoria, idCanal); 
     }
 }
--- src/services/recommendation/recommendation.service.ts
+++ src/services/recommendation/recommendation.service.ts
@@ -1,60 +1,13 @@
-import { Usuario } from "src/entities/usuario.entity";
-import { Programacao } from "src/entities/programacao.entity";
 import { Video } from "src/entities/video.entity";
-import { Categoria } from "src/entities/categoria.entity";
 import { getRepository } from "typeorm";
-import { UsuarioCategoria } from "src/entities/usuario-categoria.entity";
 import { UsuarioVideo } from "src/entities/usuario-video.entity";
-import { UsuarioService } from "../usuario.service";
-import { ProgramacaoService } from "../programacao.service";
-import { ProgramacaoVideo } from "src/entities/programacao-video.entity";
 
 export class RecommendationService{
 
-    constructor(
-        private _usuarioService:UsuarioService,
-        private _programacaoService:ProgramacaoService,
-    ){}
-
-    async recomendarProgramacoes(idUsuario:number):Promise<any>{
-
-        let usuario:Usuario = await this._usuarioService.get(idUsuario);
-
-        let categorias:Categoria[] = await this.buscarCategorias(idUsuario);
-        for(let categoria of categorias){
-            let programacao:Programacao = new Programacao({titulo: categoria.nome});
-            programacao.usuario = usuario;
-            programacao = await this._programacaoService.save(programacao);
-
-            let videosRecomendados:Video[] = await this.buscarVideosRecomendados([categoria], []);  
-            for(let videoRecomendado of videosRecomendados){
-                let programacaoVideo:ProgramacaoVideo = new ProgramacaoVideo({});
-                programacaoVideo.programacao = programacao;
-                programacaoVideo.video = videoRecomendado;
-                programacaoVideo = await this._programacaoService.addVideo(programacaoVideo);
-            }
-        }
-    }
-
-
-    async recomendarVideos(idUsuario:number):Promise<Video[]>{
-        let categorias:Categoria[] = await this.buscarCategorias(idUsuario);
+    async recomendarVideos(idUsuario:number, idCategoria:number, idCanal:number):Promise<Video[]>{
         let videosAssistidos:Video[] = await this.buscarVideosAssistidos(idUsuario);
-        let videosRecomendados:Video[] = await this.buscarVideosRecomendados(categorias, videosAssistidos);
+        let videosRecomendados:Video[] = await this.buscarVideosRecomendados(videosAssistidos, idCategoria, idCanal);
         return videosRecomendados;
-    }
-
-    async buscarCategorias(idUsuario:number):Promise<Categoria[]>{
-        let usuarioCategorias:UsuarioCategoria[] = await getRepository(UsuarioCategoria)
-            .createQueryBuilder('usuarioCategoria')
-            .innerJoinAndSelect("usuarioCategoria.usuario", "usuario")
-            .innerJoinAndSelect("usuarioCategoria.categoria", "categoria")
-            .where("usuario.id = :idUsuario", {idUsuario: idUsuario})
-            .getMany();
-
-        let categorias:Categoria[] = [];
-        usuarioCategorias.forEach((usuarioCategoria) => categorias.push(usuarioCategoria.categoria));
-        return categorias;
     }
 
     async buscarVideosAssistidos(idUsuario:number):Promise<Video[]>{
@@ -70,11 +23,8 @@ export class RecommendationService{
         return videos;
     }
 
-    async buscarVideosRecomendados(categorias:Categoria[], videosAssistidos:Video[]):Promise<Video[]>{
+    async buscarVideosRecomendados(videosAssistidos:Video[], idCategoria:number, idCanal:number):Promise<Video[]>{
     
-        let idsCategoria:number[] = [];
-        categorias.map((categoria)=>idsCategoria.push(categoria.id));
-
         let idsVideosAssistidos:number[] = [];
         videosAssistidos.map((video)=>idsVideosAssistidos.push(video.id));
 
@@ -83,11 +33,13 @@ export class RecommendationService{
             .innerJoinAndSelect("video.categoria", "categoria")
             .innerJoinAndSelect("video.canal", "canal")
             .where("");
-        if(idsCategoria.length > 0)
-            query = await query.andWhere("categoria.id IN(:...idsCategoria)", {idsCategoria: idsCategoria});
+
+        if(idCategoria && idCategoria > 0)
+            query = await query.andWhere("categoria.id = :idCategoria", {idCategoria: idCategoria});
+        if(idCanal && idCanal > 0)
+            query = await query.andWhere("canal.id = :idCanal", {idCanal: idCanal});
         if(idsVideosAssistidos.length > 0)
             query = await query.andWhere("video.id NOT IN(:...idsVideosAssistidos)", {idsVideosAssistidos: idsVideosAssistidos})
-            
         return await query.getMany();
 
     }
